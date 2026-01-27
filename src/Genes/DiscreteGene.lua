c68496678ac1f95141f0f80d2e1b120ed35031cf
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2026-01-27 21:59:02 +0800

    Create DiscreteGene.lua

---
+++ src/Genes/DiscreteGene.lua
@@ -0,0 +1,85 @@
+--[[
+
+	--------------------------------------------------------------------
+
+	Aqwam's Genetic Evolution Library (DataPredict Genetics)
+
+	Author: Aqwam Harish Aiman
+	
+	Email: aqwam.harish.aiman@gmail.com
+	
+	YouTube: https://www.youtube.com/channel/UCUrwoxv5dufEmbGsxyEUPZw
+	
+	LinkedIn: https://www.linkedin.com/in/aqwam-harish-aiman/
+	
+	--------------------------------------------------------------------
+		
+	By using this library, you agree to comply with our Terms and Conditions in the link below:
+	
+	https://github.com/AqwamCreates/DataPredict-Genetics/blob/main/docs/TermsAndConditions.md
+	
+	--------------------------------------------------------------------
+	
+	DO NOT REMOVE THIS TEXT!
+	
+	--------------------------------------------------------------------
+
+--]]
+
+local DiscreteGene = {}
+
+function DiscreteGene.new(parameterDictionary)
+
+	parameterDictionary = parameterDictionary or {}
+
+	local self = setmetatable({}, DiscreteGene)
+
+	local value = parameterDictionary.value or parameterDictionary[1] or 0
+
+	local mutationChoiceArray =  parameterDictionary.mutationChoiceArray or parameterDictionary[3] or {value}
+
+	local numberOfMutationChoices = #mutationChoiceArray
+
+	self.value = value
+
+	self.mutationChance = parameterDictionary.mutationChance or parameterDictionary[2] or 0
+
+	self.mutationChoiceArray = mutationChoiceArray
+
+	self.mutationWeightArray = parameterDictionary.mutationWeightArray or parameterDictionary[4] or table.create(numberOfMutationChoices, 1)
+
+	return self
+
+end
+
+function DiscreteGene:mutate(ignoreChance)
+
+	if (self.mutationChance <= math.random()) then return end
+
+	local mutationChoiceArray = self.mutationChoiceArray
+	
+	local mutationWeightArray = self.mutationWeightArray
+
+	local totalWeight = 0
+
+	for _, weight in ipairs(mutationWeightArray) do totalWeight = totalWeight + weight end
+
+	local randomPoint = math.random() * totalWeight
+
+	local accumulatedWeight = 0
+
+	for i, weight in ipairs(mutationWeightArray) do
+
+		accumulatedWeight = accumulatedWeight + weight
+
+		if (randomPoint <= accumulatedWeight) then
+
+			self.value = mutationChoiceArray[i]
+
+			break
+
+		end
+
+	end
+
+end
