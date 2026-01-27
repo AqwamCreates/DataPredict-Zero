d92fb595c7d9011b05d8c1f41a52c368dab3f7d3
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2026-01-27 21:58:20 +0800

    Update ContinuousGene.lua

--- src/Genes/ContinuousGene.lua
+++ src/Genes/ContinuousGene.lua
@@ -26,6 +26,16 @@
 
 --]]
 
+local mathRandom = math.random
+
+local mathSqrt = math.sqrt
+
+local mathLog = math.log
+
+local mathCos = math.cos
+
+local mathPi = math.pi
+
 local ContinuousGene = {}
 
 function ContinuousGene.new(parameterDictionary)
@@ -48,64 +58,10 @@ function ContinuousGene:mutate(ignoreChance)
 
 	if (self.mutationChance <= math.random()) then return end
 
-	local noise = self.mutationStandardDeviation * math.sqrt(-2 * math.log(math.random())) * math.cos(2 * math.pi * math.random())
-    
+	local noise = self.mutationStandardDeviation * mathSqrt(-2 * mathLog(mathRandom())) * mathCos(2 * mathPi * mathRandom())
+
 	self.value = self.value + noise
-	
-end
-
-local DiscreteGene = {}
-
-function DiscreteGene.new(parameterDictionary)
-
-	parameterDictionary = parameterDictionary or {}
-
-	local self = setmetatable({}, DiscreteGene)
-
-  local value = parameterDictionary.value or parameterDictionary[1] or 0
-
-  local mutationChoiceArray =  parameterDictionary.mutationChoiceArray or parameterDictionary[3] or {value}
-
-  local numberOfMutationChoices = #mutationChoiceArray
-
-	self.value = value
-  
-	self.mutationChance = parameterDictionary.mutationChance or parameterDictionary[2] or 0
-
-	self.mutationChoiceArray = mutationChoiceArray
-
-  	self.mutationWeightArray = parameterDictionary.mutationWeightArray or parameterDictionary[4] or table.create(numberOfMutationChoices, 1)
-
-	return self
 
 end
 
-function DiscreteGene:mutate(ignoreChance)
-
-	if (self.mutationChance <= math.random()) then return end
-
-	local mutationChoiceArray = self.mutationChoiceArray
-
-	local totalWeight = 0
-	
-    for _, weight in ipairs(self.mutationWeightArray) do totalWeight = totalWeight + weight end
-        
-    local randomPoint = math.random() * totalWeight
-	
-    local accumulatedWeight = 0
-        
-    for i, weight in ipairs(self.mutationWeightArray) do
-		
-        accumulatedWeight = accumulatedWeight + weight
-		
-        if (randomPoint <= accumulatedWeight) then
-			
-            self.value = mutationChoiceArray[i]
-			
-            break
-			
-        end
-		
-    end
-	
-end
+return ContinuousGene
