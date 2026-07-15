f888a7bb638436ceea4893ea2765769e5d4c529c
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2026-07-15 20:00:55 +0800

    Update PetriDish.lua

--- src/Genetic/PetriDish.lua
+++ src/Genetic/PetriDish.lua
@@ -2,7 +2,7 @@
 
 	--------------------------------------------------------------------
 
-	Aqwam's Evolution Library (DataPredict Evolution)
+	Aqwam's Derivative-Free Optimization Library (DataPredict Zero)
 
 	Author: Aqwam Harish Aiman
 	
@@ -16,7 +16,7 @@
 		
 	By using this library, you agree to comply with our Terms and Conditions in the link below:
 	
-	https://github.com/AqwamCreates/DataPredict-Evolution/blob/main/docs/TermsAndConditions.md
+	https://github.com/AqwamCreates/DataPredict-Zero/blob/main/docs/TermsAndConditions.md
 	
 	--------------------------------------------------------------------
 	
